554a59ddaad975e33d6b5a55c2c8c92359718bfd
Sax <42096318+Sax-Anon90@users.noreply.github.com>
2025-12-01 17:41:40 +0200

    Refactor: Change of oidc settings ti point to the deployed auth server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: false,
 
   auth: {
-    authority: 'https://localhost:7040',                   // Your auth server base URL
-    clientId: 'ed11c369fa1e49278d03caa1ab4dff1f',          // Your SPA Client ID
+    authority: 'https://pbs-security-authserver-gybthph8g6hpf4c4.southafricanorth-01.azurewebsites.net',                   // Your auth server base URL
+    clientId: 'b2f504f6ebd94655ac4a0df4bf05c35c',          // Your SPA Client ID
     redirectUri: 'http://localhost:4200/signin-oidc',      // Callback route in your Angular app
     postLogoutRedirectUri: 'http://localhost:4200',        // Where to go after logout
     scope: 'openid profile email roles offline_access',    // The scopes your server supports
